a013fcefe2ce21a287931b17066443c5af2b2bdd
QurbonbekovaSabira <sarakim2224@gmail.com>
2024-02-10 16:28:09 +0500

    trending data and images

---
+++ src/data/trending-data.js
@@ -0,0 +1,34 @@
+import img1 from "../assets/img/trending1.png"
+import img2 from "../assets/img/trending2.png"
+import img3 from "../assets/img/trending3.png"
+import img4 from "../assets/img/trending4.png"
+export const trendingData = [
+  {
+    id: 1,
+    text: "Cantilever chair",
+    salePrice: "26.00",
+    oldPrice: "42.00",
+    img:img1
+  },
+  {
+    id: 2,
+    text: "Cantilever chair",
+    salePrice: "26.00",
+    oldPrice: "42.00",
+    img:img2
+  },
+  {
+    id: 3,
+    text: "Cantilever chair",
+    salePrice: "26.00",
+    oldPrice: "42.00",
+    img:img3
+  },
+  {
+    id: 4,
+    text: "Cantilever chair",
+    salePrice: "26.00",
+    oldPrice: "42.00",
+    img:img4
+  },
+];
