1d9e5dad1b313445d8c18e123eb83629e00ead31
QurbonbekovaSabira <sarakim2224@gmail.com>
2024-02-11 21:19:17 +0500

    new change

---
+++ src/data/top-categories-data.js
@@ -0,0 +1,31 @@
+import img1 from "../assets/img/top-categories1.png";
+import img2 from "../assets/img/top-categories2.png";
+import img3 from "../assets/img/top-categories3.png";
+import img4 from "../assets/img/top-categories4.png";
+
+export const topCategoriesData = [
+  {
+    id: 1,
+    text: "Mini  LCW Chair",
+    price:"$56.00",
+    img:img1,
+  },
+  {
+    id: 2,
+    text: "Mini  LCW Chair",
+    price:"$56.00",
+    img:img2,
+  },
+  {
+    id: 3,
+    text: "Mini  LCW Chair",
+    price:"$56.00",
+    img:img3,
+  },
+  {
+    id: 4,
+    text: "Mini  LCW Chair",
+    price:"$56.00",
+    img:img4,
+  },
+];
